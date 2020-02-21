a684e03b2ca11eda91d69acfbc7f28c076b44df8
Tyagi123 <gauravtyagi888@gmail.com>
2020-02-20 18:57:29 -0800

    Initial commit

--- src/main/java/com/main/service/ValidatePyramidService.java
+++ src/main/java/com/main/service/ValidatePyramidService.java
@@ -9,18 +9,31 @@ import java.util.Map;
 public class ValidatePyramidService {
 
     public  boolean isPyramid(String str) {
+        //base case
         if(str==null || str.isEmpty()) return false;
 
-        char ch []=str.toCharArray();
-        int len=ch.length;
+        int len=str.length();
 
+        //map to store count by char
         Map<Character, Integer> cache= new HashMap<>();
 
+        Character lastChar= null;
 
+
+// creating map with count
         for(int i=0;i<len;i++){
-            Character character=ch[i];
-            Integer count= cache.get(character);
+            Character character=str.charAt(i);
+            if(lastChar==null || lastChar!=character){
+                lastChar=character;
+            }
+            else{
+                return false;
+            }
 
+            //if there is any space in string return false
+            if(character == ' ')return false;;
+
+            Integer count= cache.get(character);
             if(count==null){
                 cache.put(character,1);
             }
@@ -30,26 +43,31 @@ public class ValidatePyramidService {
         }
 
 
-        Integer lastCount=Integer.MAX_VALUE;
-
-        int index=len-1;
 
 
+        Integer lastCount=cache.get(str.charAt(len-1));
+        cache.remove(str.charAt(len-1));
+        int index=len-2;
+
+
+        //comparing from end of the string
         while(index>=0){
-            Integer count = cache.get(ch[index]);
+            Integer count = cache.get(str.charAt(index));
             if(count != null){
 
-                if(count>=lastCount){
+                if(count!=lastCount-1){
                     return  false;
                 }
                 else{
                     lastCount=count;
-                    cache.remove(ch[index]);
+                    cache.remove(str.charAt(index));
                 }
             }
 
             index--;
         }
+
+
         return true;
 
     }
